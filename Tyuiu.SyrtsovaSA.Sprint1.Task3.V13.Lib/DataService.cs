a1d8c173eb6f68f3996a8328dc05c1bebb923084
Linn <worldof.s@yandex.ru>
2024-09-18 21:41:27 +0500

    Task3

---
+++ Tyuiu.SyrtsovaSA.Sprint1.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SyrtsovaSA.Sprint1.Task3.V13.Lib
+{
+    public class DataService : ISprint1Task3V13
+    {
+        public double MultiplyOfDigits(double number)
+        {
+            int intNumber = Convert.ToInt32(number);
+            double firstDigit = (intNumber / 100) % 10;
+            double secondDigit = (intNumber / 10) % 10;
+            double lastDigit = intNumber % 10;
+            return Math.Round(firstDigit * secondDigit * lastDigit, 3);
+        }
+    }
+}
---
+++ Tyuiu.SyrtsovaSA.Sprint1.Task3.V13/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.SyrtsovaSA.Sprint1.Task3.V13.Lib;
+
+namespace Tyuiu.SyrtsovaSA.Sprint1.Task3.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Сырцова С. А. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Сырцова С. А. | ИИПб-24-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая позволяет найти произведение цифр заданного *");
+            Console.WriteLine("* числа. Ответ округлить до 3 знаков после запятой.                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a;
+
+            Console.WriteLine("Введите трехзначное число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.MultiplyOfDigits(a));
+
+            Console.ReadLine();
+        }
+    }
+}
